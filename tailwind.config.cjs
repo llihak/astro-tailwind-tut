f6067de9fa00c4d6b5fb75c93bc1809c0140f739
keith hill <llihak.hill@gmail.com>
2023-01-06 11:19:02 -0700

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
